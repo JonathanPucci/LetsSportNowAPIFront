807c72a17a17319a6495f6d73e3c3eed16af731f
JonathanPucci <jonathanpucci06@gmail.com>
2018-01-15 20:13:35 +0100

    debug

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-
-const forceSSL = function() {
-  return function(req, res, next) {
-    if (req.headers["x-forwarded-proto"] !== "https") {
-      return res.redirect(["https://", req.get("Host"), req.url].join(""));
-    }
-    next();
-  };
-};
-// ForceSSL middleware
-app.use(forceSSL());
+console.log("hey");
 app.use(express.static(__dirname + "/dist"));
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 8080;
+app.listen(port);
 
 app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname + "/dist/index.html"));
 });
+console.log("hey" + port);
